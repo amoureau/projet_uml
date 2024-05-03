a7662755f60dccf4f8583039907e7bc278e21cf9
Xelahn <alexandre.moureau@insa-lyon.fr>
2024-05-03 15:33:19 +0200

    Remove unnecessary code in User.cpp

--- src/model/User.cpp
+++ src/model/User.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //---------- RÃ©alisation de la classe <User> (fichier User.cpp) ------------
 
 //---------------------------------------------------------------- INCLUDE
@@ -46,52 +45,4 @@ User::~User ( )
 
 //------------------------------------------------------------------ PRIVE
 
-=======
-//---------- RÃ©alisation de la classe <User> (fichier User.cpp) ------------
-
-//---------------------------------------------------------------- INCLUDE
-
-//-------------------------------------------------------- Include système
-#include <string>
-using namespace std;
-
-//------------------------------------------------------ Include personnel
-#include "User.h"
-
-//------------------------------------------------------------- Constantes
-
-//----------------------------------------------------------------- PUBLIC
-
-//----------------------------------------------------- Méthodes publiques
-
-
-
-//------------------------------------------------- Surcharge d'opérateurs
-
-
-
-//-------------------------------------------- Constructeurs - destructeur
-
-User::User ()
-{
-#ifdef MAP
-    cout << "Appel au constructeur de <Catalogue>" << endl;
-#endif
-this->calculateur = ComputationAgent();
-
-} //----- Fin de User
-
-
-User::~User ( )
-{
-#ifdef MAP
-    cout << "Appel au destructeur de <Catalogue>" << endl;
-#endif
-
-} //----- Fin de ~User
-
-
-//------------------------------------------------------------------ PRIVE
-
->>>>>>> 9283a13b957d47948a4af492676ebbdc42a9b3c4
 //----------------------------------------------------- MÃ©thodes protégées
